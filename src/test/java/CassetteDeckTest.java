b69f4022961ccbfa265549e6e8685a82ae25e8d4
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-02 13:17:48 +0100

    add CassetteDeckTest class and all the test pass

---
+++ src/test/java/CassetteDeckTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+import stereo.components.CassetteDeck;
+import stereo.components.Radio;
+
+import static org.junit.Assert.assertEquals;
+
+public class CassetteDeckTest {
+    CassetteDeck cassetteDeck;
+
+    @Before
+    public void setUp(){
+        cassetteDeck = new CassetteDeck("Sony","model1");
+    }
+
+    @Test
+    public void hasMake() {
+        assertEquals("Sony", cassetteDeck.getMake());
+    }
+    @Test
+    public void hasModel() {
+        assertEquals("model1", cassetteDeck.getModel());
+    }
+}
