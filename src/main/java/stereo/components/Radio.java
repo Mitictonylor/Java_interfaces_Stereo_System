5eeed91ffcb4dfc4ab92da62a325a50e4da6d68e
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-02 12:56:02 +0100

    create radio subClass

--- src/main/java/stereo/components/Radio.java
+++ src/main/java/stereo/components/Radio.java
@@ -1,4 +1,13 @@
 package stereo.components;
 
-public class Radio {
+public class Radio extends Components{
+
+
+    public Radio(String make, String model) {
+        super(make, model);
+    }
+
+    public String tune(String nameOfRadio){
+        return nameOfRadio;
+    }
 }
