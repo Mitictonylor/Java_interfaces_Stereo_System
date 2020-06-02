1ac575a75c16362efc395f872eede115789b4ab7
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-02 12:59:14 +0100

    test 2 getters from superClass

---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+import stereo.components.Radio;
+
+import static org.junit.Assert.assertEquals;
+
+public class RadioTest {
+
+    Radio radio;
+
+    @Before
+    public void setUp(){
+        radio = new Radio("Sony","model1");
+    }
+
+    @Test
+    public void hasMake() {
+        assertEquals("Sony", radio.getMake());
+    }
+
+    @Test
+    public void hasModel() {
+        assertEquals("model1", radio.getModel());
+    }
+}
